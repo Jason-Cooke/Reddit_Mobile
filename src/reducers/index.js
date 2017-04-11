d9f6188b38f96e8c66b0846c08ca526af32b023b
Anton <anton.arboleda@gmail.com>
2017-04-10 22:32:10 -0700

    [update] - code cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import ItemsReducer from './ItemsReducer';
-import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
   items: ItemsReducer,
-}) ;
+});
