020f7997091841da209d7c4021a9ee4ba205f699
Anton <anton.arboleda@gmail.com>
2017-04-11 09:06:50 -0700

    [update] - configurestore

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { 
+  createStore, 
+  applyMiddleware, 
+  compose 
+} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { persistStore, autoRehydrate } from 'redux-persist';
@@ -8,7 +12,11 @@ const configureStore = () => {
   return createStore(
     reducers, 
     undefined, // initial state will be undefined, to let autoRehydrate to first check local storage if previous state exists
-    compose(applyMiddleware(ReduxThunk, createLogger()), 
+    compose(
+      applyMiddleware(
+        ReduxThunk, 
+        createLogger() // for development purposes to see redux actions in debugging mode
+    ), 
     autoRehydrate()) // fn from redux-persist to fill state first from local storage before fetch request is received
   );
 }
