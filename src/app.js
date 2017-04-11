ebcdd4078a8e349ba7dff37003d557bc72c795be
Anton <anton.arboleda@gmail.com>
2017-04-10 22:42:43 -0700

    [update] - cleanup

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import ItemsList from './components/ItemsList';
 
 export default class App extends Component {
   render() {
+    
     const store = createStore(reducers, undefined, compose(applyMiddleware(ReduxThunk, createLogger()), autoRehydrate()));
     persistStore(store, { storage: AsyncStorage }); //initialize storage on initial render
 
@@ -31,7 +32,7 @@ export default class App extends Component {
 }
 
 /*
-This styles was created in order to fix issue of the NavBar overlaying content on the screen
+This style was created in order to fix issue of the NavBar overlaying content on the screen
 https://github.com/aksonov/react-native-router-flux/issues/103
 */
 const styles = {
--- src/components/SelectedItem.js
+++ src/components/SelectedItem.js
@@ -5,10 +5,10 @@ import { timeDifference, validateImage } from './helpers';
 
 export default class SelectedItem extends Component {
   render() {
-    const { thumbnail, title, author, created_utc, num_comments, ups, downs, url, domain, subreddit_name_prefixed, subreddit } = this.props;
-    const { container, thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle, aligner, articleDetails, linkDetails } = styles;
+    const { thumbnail, title, author, created_utc, num_comments, ups, downs, domain, subreddit_name_prefixed, subreddit } = this.props;
+    const { itemContainer, headerContentStyle, headerTextStyle, imageStyle, articleDetails, linkDetails } = styles;
     return (
-      <View style={ container }>
+      <View style={ itemContainer }>
         <Card>
           <CardSection>
             <Image
@@ -23,7 +23,7 @@ export default class SelectedItem extends Component {
             <Text style={ articleDetails }> Written by {author} { timeDifference(created_utc) } to { subreddit_name_prefixed }</Text>
           </CardSection >
           <CardSection style={headerContentStyle}>
-            <Text style={ articleDetails }> Subreddit: {subreddit} |</Text>
+            <Text style={ articleDetails }> Subreddit: { subreddit } |</Text>
             <Text style={ linkDetails }
               onPress={() => Linking.openURL('http://' + domain)}
             > { domain }</Text>
@@ -52,28 +52,10 @@ const styles = {
     fontWeight: '600',
     color: '#657786'
   },
-  thumbnailStyle: {
-    height: 100,
-    width: 100
-  },
-  thumbnailContainerStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 10,
-    marginRight: 10
-  },
   imageStyle: {
     height: 300,
     flex: 1,
     width: 300
-  },
-  aligner: {
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 200,
-    width: 200
   },
   articleDetails: {
     fontSize: 12,
@@ -83,7 +65,7 @@ const styles = {
     color: '#657786',
     fontSize: 13
   },
-  container: { 
+  itemContainer: { 
     flex: 1, 
     backgroundColor: '#f5f8fa' 
   }
